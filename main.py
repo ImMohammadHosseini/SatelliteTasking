58c77d6f2007afa678183f83ae157abce0fae570
ImMohammadHosseini <sayedmh50@gmail.com>
2024-05-27 09:28:52 +0330

    add main for rl

--- main.py
+++ main.py
@@ -1,47 +1,17 @@
-from Basilisk.moduleTemplates import cModuleTemplate
-from Basilisk.moduleTemplates import cppModuleTemplate
-from Basilisk.utilities import SimulationBaseClass
-from Basilisk.utilities import macros
 
+from bsk_rl.envs.agile_eos import gym_env
 
-from Basilisk.utilities import SimulationBaseClass
-from Basilisk.utilities import macros
+from src.train import train_model
+from src.models.actor_model import EncoderMLPKnapsack
+from src.rl_algorithms.sacMain import sacManager
 
-def run():
-	scSim = SimulationBaseClass.SimBaseClass()
+def initializer():
+    env = gym_env
+    actor_model = EncoderMLPKnapsack
+    sacMain = 
+    return env, sacMain
 
-	#dynProcess = scSim.CreateNewProcess("name", priority)
-	dynProcess = scSim.CreateNewProcess("dynamicsProcess")
-	fswProcess = scSim.CreateNewProcess("fswProcess")
-
-	#dynProcess.addTask(scSim.CreateNewTask("name", updateRate, priority))
-	#dynProcess.addTask(scSim.CreateNewTask("name", updateRate, priority, FirstStart=delayStartTime))
-
-	dynProcess.addTask(scSim.CreateNewTask("dynamicsTask", macros.sec2nano(5.)))
-
-	mod1 = cModuleTemplate.cModuleTemplate()
-	mod1.ModelTag = "cModule1"
-
-	mod2 = cppModuleTemplate.CppModuleTemplate()
-	mod2.ModelTag = "cppModule2"
-
-	mod3 = cModuleTemplate.cModuleTemplate()
-	mod3.ModelTag = "cModule3"
-	
-	#scSim.AddModelToTask("taskName", module, priority)
-	scSim.AddModelToTask("dynamicsTask", mod1)
-	scSim.AddModelToTask("dynamicsTask", mod2, 10)
-	scSim.AddModelToTask("dynamicsTask", mod3, 5)
-
-	dynProcess.addTask(scSim.CreateNewTask("sensorTask", macros.sec2nano(10.)))
-	fswProcess.addTask(scSim.CreateNewTask("fswTask", macros.sec2nano(10.)))
-
-	scSim.InitializeSimulation()
-	scSim.ConfigureStopTime(macros.sec2nano(20.0))
-	scSim.ExecuteSimulation()
-
-	scSim.ShowExecutionOrder()
-	fig = scSim.ShowExecutionFigure(True)
-	return
-if __name__ == "__main__":
-	run()
+if __name__ == '__main__':
+    env, sacMain = initializer
+    if opts.mode == 'train':
+        train_model()
