e3f675906c23815035883fdd7534ad12a2fc69a7
ImMohammadHosseini <sayedmh50@gmail.com>
2024-05-29 13:47:20 +0330

    support for both sac and ppo

--- src/models/critic_model.py
+++ src/models/critic_model.py
@@ -19,7 +19,7 @@ class Critic_Network (nn.Module):
     ):
         super().__init__()
         self.name = name 
-        
+        self.output_dim = output_dim
         
         self.flatten = nn.Flatten()
         modules = []
@@ -27,7 +27,7 @@ class Critic_Network (nn.Module):
         if hidden_dims == None:
             main_size = 2*input_dim
             hidden_dims = []
-            last_layer = output_dim
+            last_layer = 6 if output_dim == 1 else output_dim
             while main_size > last_layer: 
                 hidden_dims.append(int(main_size))
                 main_size = int(main_size/2)
@@ -38,11 +38,17 @@ class Critic_Network (nn.Module):
                     nn.ReLU())
             )
             input_dim = h_dim
-        modules.append(nn.Sequential(nn.Linear(input_dim, output_dim), nn.Softmax()))    
+        modules.append(nn.Sequential(nn.Linear(input_dim, output_dim)))    
         self.actor = nn.Sequential(*modules)
+        self.soft_max = nn.Softmax()
     
     def forward(self, obs, *args):
-        return self.actor(self.flatten(obs))
+        x=self.actor(self.flatten(obs))
+        if self.output_dim == 1:
+            return x 
+        else :
+            return self.soft_max(x)
+        
     
     
     
